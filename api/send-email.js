df33284d9c238701ef5d371c9ed5b19ba9ff593d
roastmyprofile <contact@roastmyprofile.app>
2026-04-01 14:57:19 +0200

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,7 +1,7 @@
 // api/send-email.js
 // Schickt den vollen Roast per E-Mail – powered by Resend
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
